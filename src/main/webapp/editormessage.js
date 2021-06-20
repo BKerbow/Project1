26bc5be17917ec790135cd32b967ed55ed854744
BKerbow <bjkerbow@yahoo.com>
2021-06-20 16:10:46 -0500

    message

---
+++ src/main/webapp/editormessage.js
@@ -0,0 +1,17 @@
+let url = "http://localhost:8080/Project1/FrontController"
+
+function getData(){
+    /**This function will:
+     * 
+     * 1. Display messages from the editor on any story that
+     *    has one.
+     * 2. Display a reply box that the author can type in.
+     * 3. A submit button that transmits the data to the 
+     *    server, where it can be read by the correct
+     *    editor
+     */
+}
+
+function goBack(){
+    window.location = "/Project1/authors.html";
+}
