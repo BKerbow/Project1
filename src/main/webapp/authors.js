1016303db80f95b8e3c35e788631e33eaf5b768d
BKerbow <bjkerbow@yahoo.com>
2021-06-21 18:29:03 -0500

    added table creation

--- src/main/webapp/authors.js
+++ src/main/webapp/authors.js
@@ -15,8 +15,75 @@ function displayStories(){
         console.log(xhttp.status);
         if(xhttp.readyState == 4){
             if(xhttp.status == 200){
-                //window.location.assign('authors.html');
+                
                 console.log(xhttp.responseText);
+
+                let dataSection = document.getElementById('data');
+                dataSection.innerHTML = '';
+
+                let r = xhttp.responseText;
+                r = JSON.parse(r);
+
+                //create table
+                let authorTable = document.createElement('table');
+                authorTable.id = 'authorTable';
+
+                //create table header row
+                let thRow = document.createElement('tr');
+                let tHeaders = ['Title', 'Genre', 'Story Type', 'Description',
+                                'Tag Line', 'Completion Date', 'Approval Status', 'Reason'];
+                for (let th of tHeaders){
+                    th.innerHTML = h;
+                    thRow.appendChild(th);
+                }
+
+                authorTable.append(thRow);
+
+                //Iterate through the stories and create a table row with the date we want
+                for (let story of r){
+                let tr = document.createElement('tr');
+
+                //Title
+                let tdTitle = document.createElement('td');
+                tdTitle.innerHTML = story.title;
+                tr.appendChild(tdTitle);
+
+                //Genre
+                let tdGenre = document.createElement('td');
+                tdGenre.innerHTML = story.genre;
+                tr.appendChild(tdGenre);
+
+                //Story Type
+                let tdStoryType = document.createElement('td');
+                tdStoryType = story.type;
+                tr.appendChild(tdStory);
+
+                //Description
+                let tdDescription = document.createElement('td');
+                tdDescription = story.description;
+                tr.appendChild(tdDescription);
+
+                //Tag Line
+                let tdTagLine = document.createElement('td');
+                tdTagLine = story.tagLine;
+                tr.appendChild(tdTagLine);
+
+                //Completion Date
+                let tdCompletionDate = document.createElement('td');
+                tdCompletionDate = story.completionDate;
+                tr.appendChild(tdCompletionDate);
+                
+                //Approval Status
+                let tdApprovalStatus = document.createElement('td');
+                tdApprovalStatus = story.approvalStatus;
+                tr.appendChild(tdApprovalStatus);
+
+                //Reason
+                let tdReason = document.createElement('td');
+                tdReason = story.reason;
+                tr.appendChild(reason);
+                }
+                
             }
         }
     }
