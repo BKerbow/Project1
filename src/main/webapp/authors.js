5ca1bbcdc7a89011babb65fadce8bacd2d7643d3
BKerbow <bjkerbow@yahoo.com>
2021-06-21 22:03:56 -0500

    almost all author services now working

--- src/main/webapp/authors.js
+++ src/main/webapp/authors.js
@@ -95,37 +95,6 @@ function displayStories(){
     }
 }
 
-function getStories(){
-    //Everything happening here (outside of onreadystatechange) prepares data and sends it to server
-    console.log("clicked Story Submit Button");
-
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.open("POST", url + "/story_submit", true);
-    xhttp.send();
-
-    console.log("sent json");
-    console.log("The xhttp ready state is: " + xhttp.readyState);
-    console.log("The xhttp status is: " + xhttp.status);
-
-    xhttp.onreadystatechange = receiveData;
-    
-    function receiveData(){
-        console.log("The xhttp ready state is: " + xhttp.readyState);
-        console.log("The xhttp status is: " + xhttp.status);
-        if (xhttp.readyState == 4){
-            if(xhttp.status == 200){
-                //test to see if javascript is talking
-                console.log("Request is ready and sending.");
-                //load new html page sent from servlet
-                console.log(xhttp.responseText);
-                window.location.href = xhttp.responseText;
-                console.log("Switching to newwork.html!");
-            }
-        }
-    }
-}
-
 function getEditorMessages(){
     console.log("displaying editor's messages");
     let xhttp = new XMLHttpRequest();
